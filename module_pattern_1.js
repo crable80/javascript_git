ef82cd4d6a116da40239c60ce2713c3168081080
crable <crable@naver.com>
2022-12-05 16:48:25 +0900

    모듈패턴 1 완료

---
+++ module_pattern_1.js
@@ -0,0 +1,35 @@
+//클로저 (내부변수, 내부함수) - 인스턴스 여러개 생성가능
+var module = (function () {
+    /*------------------------------
+    * 모듈 패턴을 구현한  클로저 코드
+    *-------------------------------*/
+    
+    //은닉될 멤버 정의
+    var privateKey = 0;
+    function privateMethod(){
+        return ++privateKey;
+    }
+
+    //공개될 멤버 정의
+    return {
+        publicKey : privateKey,
+        publicMethod : function(){
+            return privateMethod();
+        }
+    }
+//})(); //즉시실행
+});
+
+//console.log(module.publicMethod()); // 즉시실행 함수일때 (인스턴스 하나)
+
+//다중 인스턴스
+//객체1
+var obj1 = module();
+console.log('obj1.publicMethod(): ', obj1.publicMethod());
+console.log('obj1.publicMethod(): ', obj1.publicMethod());
+
+//객체2
+var obj2 = module();
+console.log('obj2.publicMethod(): ', obj2.publicMethod());
+console.log('obj2.publicMethod(): ', obj2.publicMethod());
+
