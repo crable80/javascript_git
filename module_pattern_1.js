eb0923e1f04e22526a5f457cabc8480c356beeb1
crable <crable@naver.com>
2022-12-23 16:52:12 +0900

    비공개 멤버의 헛점

--- module_pattern_1.js
+++ module_pattern_1.js
@@ -1,3 +1,4 @@
+/* 모듈패턴 #1 ---------------------------------------------------------------------*/
 //클로저 (내부변수, 내부함수) - 인스턴스 여러개 생성가능
 var module = (function () {
     /*------------------------------
@@ -22,14 +23,27 @@ var module = (function () {
 
 //console.log(module.publicMethod()); // 즉시실행 함수일때 (인스턴스 하나)
 
-//다중 인스턴스
+//다중 인스턴스 예제
 //객체1
 var obj1 = module();
 console.log('obj1.publicMethod(): ', obj1.publicMethod());
 console.log('obj1.publicMethod(): ', obj1.publicMethod());
-
 //객체2
 var obj2 = module();
 console.log('obj2.publicMethod(): ', obj2.publicMethod());
 console.log('obj2.publicMethod(): ', obj2.publicMethod());
 
+
+
+/* 모듈패턴 #2 ---------------------------------------------------------------------*/
+//#네임스페이스
+// 위험하다
+var MYAPP = {};
+
+//개선안
+if(typeof MYAPP === 'undefined'){
+    MYAPP = {};
+}
+
+// 또는 더 짧게 작성가능
+var MYAPP = MYAPP || {};
